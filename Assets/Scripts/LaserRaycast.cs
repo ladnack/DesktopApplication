d8237c4fae1726336e5ec976f0c56e0e9fbae6d4
Kyohei-Sakai <nico_f00tb@yahoo.co.jp>
2017-02-08 23:41:43 +0900

    Add script to attach raycast to controller

---
+++ Assets/Scripts/LaserRaycast.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserRaycast : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, transform.forward);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            Transform objectHit = hit.transform;
+
+            // Do something with the object that was hit by the raycast.
+            Debug.Log("Hit " + objectHit.gameObject);
+
+        }
+
+    }
+}
