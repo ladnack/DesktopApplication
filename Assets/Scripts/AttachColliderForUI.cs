aab19d89819fefb2568f8721106221dea4f5f413
Kyohei-Sakai <nico_f00tb@yahoo.co.jp>
2017-02-09 23:04:00 +0900

    Make new canvas

---
+++ Assets/Scripts/AttachColliderForUI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttachColliderForUI : MonoBehaviour {
+
+    public Canvas canvas;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        // 親であるCanvasにColliderをアタッチ
+        canvas.gameObject.AddComponent<BoxCollider>();
+        // 子であるUIにColliderをアタッチ
+        foreach (Transform child in canvas.transform)
+        {
+            child.gameObject.AddComponent<BoxCollider>();
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
